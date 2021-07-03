11fc28f4bf16680f84aaf9a6021bc67cd2c0a390
Lucas Alexander <lucasalexander13@hotmail.com>
2021-07-03 11:40:12 -0300

    Classe de estruturas adicionada

---
+++ Exemplos/Estruturas.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Fundamentos.Exemplos
+{
+    class Estruturas
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine('Hello World');
+        }
+    }
+}
