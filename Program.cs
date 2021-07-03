6a5417361cf2da56fbfb4dfe053bb9615a975c70
Lucas Alexander <lucasalexander13@hotmail.com>
2021-07-02 21:35:30 -0300

    Added Program class

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Fundamentos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String fname, lname;
+            Console.Write("Enter your first name: ");
+            fname = Console.ReadLine();
+            Console.Write("Enter your last name: ");
+            lname = Console.ReadLine();
+            Console.WriteLine("Your name is {0} {1}.", fname, lname);
+        }
+    }
+}
