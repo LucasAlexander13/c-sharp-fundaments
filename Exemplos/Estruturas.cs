d2656329bdc356905c047531a3aff05e703b9721
Lucas Alexander <lucasalexander13@hotmail.com>
2021-07-07 13:29:00 -0300

    Adicionada instrução Try, Catch, Finally e Throw

--- Exemplos/Estruturas.cs
+++ Exemplos/Estruturas.cs
@@ -124,5 +124,40 @@ namespace Fundamentos.Exemplos
             return;
         }
 
+        static void InstrucoesTryCatchFinallyThrow(string[] args)
+        {
+            double Dividir(double x, double y)
+            {
+                if (y == 0)
+                    throw new DivideByZeroException();
+
+                return x / y;
+            }
+
+            try
+            {
+                if (args.Length != 2)
+                {
+                    throw new InvalidOperationException("Informe 2 números.");
+                }
+
+                double x = double.Parse(args[0]);
+                double y = double.Parse(args[1]);
+                Console.WriteLine(Dividir(x, y));
+            }
+            catch (InvalidOperationException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Erro genérico: {e.Message}");
+            }
+            finally
+            {
+                Console.WriteLine("Até breve!");
+            }
+        }
+
     }
 }
